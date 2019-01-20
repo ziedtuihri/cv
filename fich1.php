494340028ee6ff3841eae52d27f095471d8118d4
zied <zied.hack@gmail.com>
2019-01-20 14:17:40 +0100

    ..

--- fich1.php
+++ fich1.php
@@ -1,12 +1,12 @@
 <?php
-$test = file_get_contents("infor.json");
+$test = file_get_contents("info.json");
 $json=json_decode($test,true);
 
 if(isset($_POST["ch"]) && isset($_POST["x"])){
 	$ch=($_POST["ch"]);$x=($_POST["x"]);
 	$json[0][$x]=$ch;
 		$json = json_encode($json);
-		$test=file_put_contents('infor.json',$json);
+		$test=file_put_contents('info.json',$json);
 		$json=json_decode($test,true);
 		
 }
