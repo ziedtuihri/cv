e4260f284bfc841a72150fe0d5811774e9d07938
zied <zied.hack@gmail.com>
2019-01-20 13:32:24 +0100

    del

--- fich1.php
+++ fich1.php
@@ -10,33 +10,4 @@ if(isset($_POST["ch"]) && isset($_POST["x"])){
 		$json=json_decode($test,true);
 		
 }
- /*
- foreach($json[0]  as $R=>$D){
-echo $D."<br>";
- }
- 
-foreach($json  as $R=>$D)
-$D['T1'];
- 
-echo "<table>";
-foreach($json  as $R=>$D){
- echo "<tr id='Tr_".$R."'>"; 
- foreach($D as $key=>$Value){
-    echo "<td id='Td_".$R."_".$key."'>".$Value."</td>";
-  }
- echo "</tr>";
-}
-echo "</table>";
-
-while($item = array_shift($json))
-{
-    foreach ($item as $key => $value)
-    {
-		echo "<pre>";
-        echo $key.' => '.$value."\n";
-echo "</pre>"; 
-    }
-}
-*/
- 
 ?>
